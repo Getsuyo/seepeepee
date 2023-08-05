55850bba77e59e6cc2635d8dd754698751b110c4
Getsuyo <140719551+Getsuyo@users.noreply.github.com>
2023-08-05 17:59:59 +0545

    Triangle!

--- seapp/src/main.cpp
+++ seapp/src/main.cpp
@@ -1,9 +1,48 @@
 #include<iostream>
+#include <GLFW/glfw3.h>
 #include"fn.h"
 
 int main()
 {
-	Log("Hello, World!");
+	//Log("Hello, World!");
 	
-	std::cin.get();
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5, -0.5);
+        glVertex2f(0.0, 0.5);
+        glVertex2f(0.5, -0.5);
+        
+        glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
