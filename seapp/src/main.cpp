ead4f92910066c9fb2c199682788f8869524b417
Getsuyo <140719551+Getsuyo@users.noreply.github.com>
2023-08-07 15:53:30 +0545

    idk lol

--- seapp/src/main.cpp
+++ seapp/src/main.cpp
@@ -1,47 +1,27 @@
 #include <GLEW/glew.h>
 #include <GLFW/glfw3.h>
 #include "fn.h"
-
-static unsigned int CompileShader(unsigned int type, const std::string& source) {
-    unsigned int id = glCreateShader(type);
-    const char* src = source.c_str();
-    glShaderSource(id, 1, &src, nullptr);
-    glCompileShader(id);
-
-    int result;
-    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-    if (result == GL_FALSE) {
-        int length;
-        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-        char* message = (char*)alloca(length * sizeof(char));
-        glGetShaderInfoLog(id, length, &length, message);
-        std::cout << "FAILED: To Compile " << (type == GL_VERTEX_SHADER ? "Vertex " : "Fragment ") << "Shader" << std::endl;
-        std::cout << message << std::endl;
-        glDeleteShader(id);
-        return 0;
-    }
-    return id;
-}
-
-static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
-    unsigned int program = glCreateProgram();
-    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
-    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
-
-
-    glAttachShader(program, vs);
-    glAttachShader(program, fs);
-    glLinkProgram(program);
-    glValidateProgram(program);
-
-    glDeleteShader(vs);
-    glDeleteShader(fs);
-
-    return program;
-}
+/*
+static unsigned int CompileShader(unsigned int, const std::string&);
+static unsigned int CreateShader(const std::string&, const std::string&);
+*/
 
 int main()
 {
+
+
+
+
+
+    //I might wanna master cpp before OpenGL or even just finish my school pending works OR BEAT THAT KNIGHT THAT'S "HOLLOW"
+
+
+
+
+
+
+
+#ifdef 0
     GLFWwindow* window;
 
     /* Initialize the library */
@@ -115,5 +95,5 @@ int main()
 
     glDeleteProgram(shader);
     glfwTerminate();
-    return 0;
+#endif
 }
